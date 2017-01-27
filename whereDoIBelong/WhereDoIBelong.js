c042df733ffd72120f2444136f85febf36fd92d2
Otto Kafka <ottokafka@yahoo.com>
2017-01-27 14:10:58 -0800

    Complete algorithm

--- whereDoIBelong/WhereDoIBelong.js
+++ whereDoIBelong/WhereDoIBelong.js
@@ -1,11 +1,42 @@
 /**
  * Created by automac on 1/26/17.
+ * Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+ For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+ Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
  */
 
 
-function getIndexToIns(arr, num) {
-    // Find my place in this sorted array.
-    return num;
-}
+function getIndexToIns(arr, num){
 
+var pushNumToArray = arr.push(num);
+    console.log(pushNumToArray);
+
+    var arraySort = arr.sort(function(a, b){
+        return a-b
+    });
+
+    console.log(arraySort);
+
+    var getIndexOf = arraySort.indexOf(num);
+    console.log("the final product is: "+getIndexOf);
+
+    return getIndexOf;
+}
 getIndexToIns([40, 60], 50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
