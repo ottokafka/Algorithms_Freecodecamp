728ba35524205ecb8c852734fba354c6e45bd816
Otto Kafka <ottokafka@yahoo.com>
2017-01-31 14:36:25 -0800

    Completed algorithm with comments

--- whereArtThou/whereArtThou.js
+++ whereArtThou/whereArtThou.js
@@ -2,13 +2,24 @@
  * Created by automac on 1/31/17.
  */
 function whatIsInAName(collection, source) {
-    // What's in a name?
-    var arr = [];
-    // Only change code below this line
 
+// Object keys method returns the arrays own property's in this case it returns: ["last"] from the second array source
+        var sourceProperty = Object.keys(source);
 
-    // Only change code above this line
-    return arr;
-}
+//filter is ES6 language filter method will create a new array with all the elements that pass the test
+    //in this case we have filter namesInCollection. the arrow function => is just another way of writing the keyword function
+    return collection.filter((namesInCollection) => {
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+//the every method tests whether all elements in the array pass the test provided by the provided function
+        return sourceProperty.every((propertyInCollection) => {
+
+//here we test equality with the function namesInCollection[property] === source[property]
+            return namesInCollection[propertyInCollection] === source[propertyInCollection];
+
+        });
+    });
+    }
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null },
+        { first: "Tybalt", last: "Capulet" }],
+    { last: "Capulet" });
