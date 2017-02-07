8872c8e00e8f45da4f674ad92ae88caa763bbdaf
Otto Kafka <ottokafka@yahoo.com>
2017-02-06 19:42:42 -0800

    Completed algorithm with comments

--- missing letters/missing letter 1.js
+++ missing letters/missing letter 1.js
@@ -2,8 +2,12 @@ function fearNotLetter(str) {
 
     for(var i = 0; i < str.length; i++) {
         var charCode = str.charCodeAt(i);
+        console.log(charCode); //97, 98, 99, 101
         if (charCode !== str.charCodeAt(0) + i) {
+            // if not 97, 98. 99, 101 + 1
+            console.log(String.fromCharCode(charCode - 1));
             return String.fromCharCode(charCode - 1);
+
         }
     }
     return undefined;
